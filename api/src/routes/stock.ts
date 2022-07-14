b483315b101059789a8174a5d15170ea9ace8a24
Amolo <railamolo@gmail.com>
2022-07-14 14:48:11 +0300

    updated order endpoints

--- api/src/routes/stock.ts
+++ api/src/routes/stock.ts
@@ -19,6 +19,8 @@ router.post("/", [requireJWT], async (req: Request, res: Response) => {
         if (decodedSession.type == 'valid') {
             let userId = decodedSession.session.userId
             let { unPasteurized, pasteurized, dhmType } = req.body;
+            unPasteurized = parseFloat(unPasteurized)
+            pasteurized = parseFloat(pasteurized)
             let entry = await db.stockEntry.create({
                 data: {
                     pasteurized: parseFloat(pasteurized), unPasteurized: parseFloat(unPasteurized),
@@ -96,6 +98,7 @@ router.post("/order", [requireJWT], async (req: Request, res: Response) => {
         if (decodedSession.type == 'valid') {
             let userId = decodedSession.session.userId
             let { dhmType, dhmVolume, remarks, orderId } = req.body;
+            dhmVolume = parseFloat(dhmVolume)
             let order = await db.order.create({
                 data: {
                     dhmType, dhmVolume: dhmVolume, remarks, status: "Dispensed",
@@ -114,128 +117,6 @@ router.post("/order", [requireJWT], async (req: Request, res: Response) => {
 });
 
 
-// Register
-router.post("/reset-password", async (req: Request, res: Response) => {
-    try {
-        let { username, email, id } = req.body;
-        if (email && !validateEmail(email)) {
-            res.statusCode = 400
-            res.json({ status: "error", message: "invalid email value provided" })
-            return
-        }
-        // Initiate password reset.
-        let user = await db.user.findFirst({
-            where: {
-                ...(email) && { email },
-                ...(username) && { username },
-                ...(id) && { id }
-            }
-        })
-
-        let session = encodeSession(process.env['SECRET_KEY'] as string, {
-            createdAt: ((new Date().getTime() * 10000) + 621355968000000000),
-            userId: user?.id as string,
-            role: "RESET_TOKEN"
-        })
-        user = await db.user.update({
-            where: {
-                ...(email) && { email },
-                ...(username) && { username },
-                ...(id) && { id }
-            },
-            data: {
-                resetToken: session.token,
-                verified: false,
-                resetTokenExpiresAt: new Date(session.expires)
-            }
-        })
-        res.statusCode = 200
-        let resetUrl = `${process.env['WEB_URL']}/new-password?id=${user?.id}&token=${user?.resetToken}`
-        console.log(resetUrl)
-        let response = await sendPasswordResetEmail(user, resetUrl)
-        console.log(response)
-        res.json({ message: `Password reset instructions have been sent to your email, ${user?.email}`, status: "success", });
-        return
-
-    } catch (error: any) {
-        console.log(error)
-        res.statusCode = 401
-        if (error.code === 'P2025') {
-            res.json({ error: `Password reset instructions have been sent to your email`, status: "error" });
-            return
-        }
-        res.json({ error: error, status: "error" });
-        return
-    }
-
-});
-
-// Set New Password
-router.post("/new-password", [requireJWT], async (req: Request, res: Response) => {
-    try {
-        let { password, id } = req.body;
-        let user = await db.user.findFirst({
-            where: {
-                id: id as string
-            }
-        })
-        let token = req.headers.authorization || '';
-        let decodedSession = decodeSession(process.env['SECRET_KEY'] as string, token.split(" ")[1] as string)
-        if ((decodedSession.type !== 'valid') || !(user?.resetToken) || ((user?.resetTokenExpiresAt as Date) < new Date())
-            || (decodedSession.session?.role !== 'RESET_TOKEN')
-        ) {
-            res.statusCode = 401
-            res.json({ error: `Invalid and/or expired password reset token. Code: ${decodedSession.type}`, status: "error" });
-            return
-        }
-        let salt = await bcrypt.genSalt(10)
-        let _password = await bcrypt.hash(password, salt)
-        let response = await db.user.update({
-            where: {
-                id: id as string
-            },
-            data: {
-                password: _password, salt: salt, resetToken: null, resetTokenExpiresAt: null, verified: true
-            }
-        })
-        console.log(response)
-        res.statusCode = 200
-        res.json({ message: "Password Reset Successfully", status: "success" });
-        return
-    } catch (error) {
-        console.log(error)
-        res.statusCode = 401
-        res.json({ error: error, status: "error" });
-        return
-    }
-
-});
-
-
-// Delete User
-router.delete("/:id", async (req: Request, res: Response) => {
-    try {
-        let { id } = req.params;
-        let user = await db.user.delete({
-            where: {
-                id: id
-            }
-        })
-        let responseData = user
-        res.statusCode = 201
-        res.json({ user: responseData, status: "success" })
-        return
-    } catch (error: any) {
-        res.statusCode = 400
-        console.error(error)
-        if (error.code === 'P2002') {
-            res.json({ status: "error", error: `User with the ${error.meta.target} provided already exists` });
-            return
-        }
-        res.json(error)
-        return
-    }
-});
 
 
 
